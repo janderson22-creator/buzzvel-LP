93598a416bd02fc1f7e913059828ee6dec59abd0
janderson22-creator <jandersomcosta@yahoo.com.br>
2024-10-29 17:37:36 -0300

    fix name imports

--- app/data/index.tsx
+++ app/data/index.tsx
@@ -1,8 +1,8 @@
-export { personalizedServices } from './personalizedServices';
-export { powerfulFeatures } from './powerfulFeatures';
 export { intro } from './intro';
 export { pickTheSun } from './pickTheSun';
 export { makeSomething } from './makeSomething';
 export { allThePower } from './allThePower';
 export { header } from './header';
 export { footer } from './footer';
+export { personalizedServices } from './PersonalizedService';
+export { powerfulFeatures } from './powerfulFeatureds';
